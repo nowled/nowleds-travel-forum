68e4200609e3b9ca3c1c3d7f140756f87f0f456e
Charles Peterson <nowled@excite.com>
2017-09-23 23:03:42 +0300

    Finished the edit and destroy action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,13 @@
 class MessagesController < ApplicationController
+    
+    before_action :find_message, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@messages = Message.all.order("created_at DESC")
 	end
 
 	def show
-     @message = Message.find(params[:id])
+    
 	end 
 
 	def new
@@ -21,10 +23,33 @@ class MessagesController < ApplicationController
 		end 
 	end
 
+	def edit
+		#this is only used to view the messages
+      
+	end
+
+	 def update
+	 	if @message.update(message_params)
+	 		redirect_to message_path
+	 	else
+	 		render 'edit'
+	 	end 
+	 end
+
+	 def destroy
+	 	@message.destroy
+	 	redirect_to root_path
+
+	 end 
+
 	private
 
 	def message_params
 		params.require(:message).permit(:title, :description)
 
 	end 
+
+	def find_message
+     @message = Message.find(params[:id])
+	end
 end
